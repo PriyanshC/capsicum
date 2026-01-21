6f75e415d429d42fe746c0aae1cc5bacb0d53ae5
Priyansh <priyanshchugh@outlook.com>
2026-01-21 00:05:48 +0000

    Upd tl example

--- src/main/scala/example/TurboLiftDemo.scala
+++ src/main/scala/example/TurboLiftDemo.scala
@@ -4,31 +4,28 @@ import turbolift._
 import turbolift.Extensions._
 // import language.experimental.captureChecking
 
-sealed trait TLProducerSignature extends Signature {
+sealed trait FnProducerSignature extends Signature {
     def produce(): (() => Unit) !! ThisEffect
 }
 
-trait TLProducerEffect extends Effect[TLProducerSignature] with TLProducerSignature:
+trait FnProducerEffect extends Effect[FnProducerSignature] with FnProducerSignature:
     final override def produce() = perform(_.produce())
 
-extension (px: TLProducerEffect)
+extension (px: FnProducerEffect)
     def noop = 
-        new px.impl.Stateless[Identity, Option, Any] with px.impl.Sequential with TLProducerSignature {
+        new px.impl.Stateless[Identity, Option, Any] with px.impl.Sequential with FnProducerSignature {
             override def onReturn(a: Unknown) = !!.pure(Some(a))
-            // override def produce() = !!.pure(() => ())
-            override def produce() = {println("No-op function called!"); !!.pure(this.produce)} // Leak?
+            override def produce() = !!.pure(() => ())
+            // override def produce() = {println("No-op function called!"); !!.pure(this.produce)} // Leak?
         }.toHandler
 
-case object MyTLProducer extends TLProducerEffect
-type MyTLProducer = MyTLProducer.type
+case object MyFnProducer extends FnProducerEffect
+type MyFnProducer = MyFnProducer.type
 
 object TurboLiftDemo extends App {
-    val program =
-        for
-            f <- MyTLProducer.produce()
-        yield (f)
+    val program = MyFnProducer.produce()
         
-    val cont = program.handleWith(MyTLProducer.noop).run
+    val cont = program.handleWith(MyFnProducer.noop).run
 
     println(cont)
     val a = cont.map(_())
