0671261a704f4e4678aa36fec10a2399787bb006
Priyansh <priyanshchugh@outlook.com>
2026-02-22 20:10:35 +0000

    cc

--- src/main/scala/capsicum/Capsicum.scala
+++ src/main/scala/capsicum/Capsicum.scala
@@ -14,7 +14,16 @@ import language.experimental.captureChecking
 //   }
 // }
 
-enum Prog[+R, +A, -V] {
+enum Prog[+R, A, V] {
   case Pure(y: R)
   case Suspend(x: A, resume: V -> Prog[R, A, V])
+
+  def map[B](f: R -> B): Prog[B, A, V] = this match {
+    case Pure(v) => Pure(f(v))
+    case Suspend(req, k) => Suspend(req, t => k(t).map(f))
+  }
+
+  def flatMap[B](f: R -> Prog[B, A, V]): Prog[B, A, V] = this match
+    case Pure(v) => f(v)
+    case Suspend(req, k) => Suspend(req, t => k(t).flatMap(f))
 }
