6abe8006bd74dc817bd457afe2240ffd451dae43
Priyansh <priyanshchugh@outlook.com>
2026-02-25 20:22:21 +0000

    example program a1

--- src/main/scala/capsicum/CapsicumDemo.scala
+++ src/main/scala/capsicum/CapsicumDemo.scala
@@ -11,17 +11,36 @@ type HandlerReturn = Int
 def run(prog: Prog[ProgReturn, HandlerInput, ResumeValue]): HandlerReturn = prog match {
   case Prog.Pure(result) => 
     result
-  case Prog.Suspend(add, k) =>
+  case Prog.Suspend(add, k) => {
     run(k(true).flatMap { x =>
       k(false).map { y => 
         if (add) then x + y else x - y
       }
     })
+  }
     // val x = run(k(true))
     // val y = run(k(false))
     // if (add) then x + y else x - y
 }
 
+object A1 {
+  type C = Int
+  val N = 10
+  // if (f: () => Unit)
+  // capsicum.Prog[Unit, Unit, () => Unit] captures the root capability `cap` in invariant position. This capability cannot be converted to an existential in the result type of a function.
+
+  // if progA1 isnt in params its fine, otherwise bad
+  val progA1 = Prog.Suspend((), (f: () -> Unit) => Prog.Pure((1 to N).foreach(_ => f())))
+  def runA1(): Unit = {
+    println("Handler called!")
+    val prog: Prog[Unit, Unit, () -> Unit] = progA1
+    prog match
+      case Prog.Pure(y) => y
+      case Prog.Suspend(_, k) => {
+        k(runA1)
+      }
+  }
+}
 
 object Main extends App {
 
