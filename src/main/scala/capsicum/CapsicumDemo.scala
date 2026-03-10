a999245fbf2a91af38f210008b2741c0e14f72b2
Priyansh <priyanshchugh@outlook.com>
2026-03-10 11:44:40 +0000

    leaky cont example

--- src/main/scala/capsicum/CapsicumDemo.scala
+++ src/main/scala/capsicum/CapsicumDemo.scala
@@ -31,6 +31,51 @@ lazy object SimpleDemo {
 }
 
 
+// effect Producer[T]{
+// def produce():T
+// }
+
+// def produceAndCallN(n:Int)(usingfunProducer:Producer[()=>()]):Unit={
+// val f=funProducer.produce()
+//  (1 to n).foreach(_=>f())
+// }
+
+// val unsafeHandler:Handler[Producer[()=>()]]={resume=>
+// println("Handler␣called!")
+// resume(resume)//Leak!!
+// }
+
+// run(produceAndCallN(10)).withHandler(unsafeHandler)
+
+
+lazy object ContinuationLeakDemo {
+
+  type FnProducer = Handler[Unit, () -> Unit, Unit, Unit]
+
+  def program(n: Int): FnProducer ?-> Unit = {
+    summon[FnProducer].handle((), { f =>
+      (1 to n).foreach(_ => f())
+    })
+  }
+
+  val goodHandler: FnProducer = new Handler({_ => { resume => 
+    println("Simple handler invoked!")
+    resume(() => ())
+  }})
+
+  // val badHandler: FnProducer = new Handler({_ => { resume => 
+  //   println("Unsafe handler invoked!")
+  //   resume(() => {
+  //     resume(() => ())
+  //   })
+  // }})
+  val result: Unit = run(program(5))(goodHandler)
+}
+
+
 object Main extends App {
-  println(SimpleDemo.result)
+  // val result = SimpleDemo.result
+  val result = ContinuationLeakDemo.result
+
+  println(result)
 }
