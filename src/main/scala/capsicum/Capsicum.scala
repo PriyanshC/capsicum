81e7695e627efddc7552c4fd39cc31484614cc53
Priyansh <priyanshchugh@outlook.com>
2026-04-08 18:28:37 +0100

    higher-kinded type in Handler to allow multi-effect

--- src/main/scala/capsicum/Capsicum.scala
+++ src/main/scala/capsicum/Capsicum.scala
@@ -2,16 +2,41 @@ package capsicum
 
 import language.experimental.captureChecking
 
+trait Effect[+V]
 
-// HandlerIn, ResumeIn, ResumeOut, ProgramOut
-abstract case class Handler[I, V, P, R]() extends caps.ExclusiveCapability {
-  def handle(x: I, resume: =>(V => P)): R
+abstract class Handler[E[+_], P, R]() extends caps.ExclusiveCapability {
+  def handle[V](op: E[V], resume: => V => P): R
 }
 
 object Handler {
-  def apply[I, V, P, R, C^](f: (I -> ((V => P) -> R))^{C}): Handler[I, V, P, R]^{C} = new Handler {
-    final override def handle(x: I, resume: =>(V => P)): R = f(x)(resume)
-  }
+  def apply[E[+_], P, R, C^](f: ([V] => (E[V], (V => P)) => R)^{C}): Handler[E, P, R]^{C} =
+      new Handler[E, P, R] {
+        override def handle[V](op: E[V], resume: => V => P): R = f[V](op, resume)
+      }
 }
 
-def run[I, V, P, R](handler: Handler[I, V, P, R]^)(prog: (Handler[I, V, P, R]^{handler}) ?-> R): R = prog(using handler)
+// note to self: check against `prog ?-> P`
+def run[E[+_], P, R](handler: Handler[E, P, R]^)(prog: (Handler[E, P, R]^{handler}) ?-> R): R = prog(using handler)
+
+object State {
+  enum StateOp[S, +R] extends Effect[R] {
+    case Get[S]() extends StateOp[S, S]
+    case Put[S](value: S) extends StateOp[S, Unit]
+  }
+
+  def newState[S, R](initial: S): Handler[[V] =>> StateOp[S, V], R, R] = {
+    var state: S = initial
+
+    new Handler[[V] =>> StateOp[S, V], R, R] {
+      override def handle[V](op: StateOp[S, V], resume: => V => R): R = 
+        op match {
+          case StateOp.Get() => 
+            resume(state.asInstanceOf[V])
+            
+          case StateOp.Put(newValue) => 
+            state = newValue.asInstanceOf[S]
+            resume(().asInstanceOf[V])
+        }
+    }
+  }
+}
--- src/main/scala/capsicum/CapsicumDemo.scala
+++ src/main/scala/capsicum/CapsicumDemo.scala
@@ -1,99 +1,99 @@
-package capsicum.demo
+// package capsicum.demo
 
-import language.experimental.captureChecking
-import capsicum._
+// import language.experimental.captureChecking
+// import capsicum._
 
-lazy object SimpleDemo {
-  type ResumeIn = Boolean
-  type ResumeOut = Int
-  type HandlerIn = Unit
-  type ProgramOut = Int
+// lazy object SimpleDemo {
+//   type ResumeIn = Boolean
+//   type ResumeOut = Int
+//   type HandlerIn = Unit
+//   type ProgramOut = Int
 
-  type MyHandler = Handler[HandlerIn, ResumeIn, ResumeOut, ProgramOut]
+//   type MyHandler = Handler[HandlerIn, ResumeIn, ResumeOut, ProgramOut]
 
-  val programResume: ResumeIn -> ResumeOut = { (flag: ResumeIn) => 
-    if (flag) {1} else {0}
-  }
+//   val programResume: ResumeIn -> ResumeOut = { (flag: ResumeIn) => 
+//     if (flag) {1} else {0}
+//   }
 
-  val programHandler: MyHandler = Handler({(_: HandlerIn) => { (resume: (ResumeIn => ResumeOut)) => 
-    val x: Int = resume(false)
-    val y: Int = resume(true)
-    x + y
-  }})
+//   val programHandler: MyHandler = Handler({(_: HandlerIn) => { (resume: (ResumeIn => ResumeOut)) => 
+//     val x: Int = resume(false)
+//     val y: Int = resume(true)
+//     x + y
+//   }})
 
 
-  val program: MyHandler ?-> ProgramOut = {
-    val h = summon[MyHandler]
-    h.handle((), programResume)
-  }
+//   val program: MyHandler ?-> ProgramOut = {
+//     val h = summon[MyHandler]
+//     h.handle((), programResume)
+//   }
 
-  val result: ProgramOut = run(programHandler)(program)
-}
+//   val result: ProgramOut = run(programHandler)(program)
+// }
 
-lazy object ContinuationLeakDemo {
+// lazy object ContinuationLeakDemo {
 
-  type FnProducer = Handler[Unit, () -> Unit, Unit, Unit]
+//   type FnProducer = Handler[Unit, () -> Unit, Unit, Unit]
 
-  def program(n: Int): FnProducer ?-> Unit = {
-    summon[FnProducer].handle((), { f =>
-      (1 to n).foreach(_ => f())
-    })
-  }
+//   def program(n: Int): FnProducer ?-> Unit = {
+//     summon[FnProducer].handle((), { f =>
+//       (1 to n).foreach(_ => f())
+//     })
+//   }
 
-  val goodHandler: FnProducer = Handler({_ => { resume => 
-    println("Simple handler invoked!")
-    resume(() => ())
-  }})
+//   val goodHandler: FnProducer = Handler({_ => { resume => 
+//     println("Simple handler invoked!")
+//     resume(() => ())
+//   }})
 
-  // ERROR: (only if resume is defined as capturing, is this right?)
-  // val badHandler: FnProducer = Handler({_ => { resume => 
-  //   println("Unsafe handler invoked!")
-  //   resume(() => {
-  //     resume(() => ())
-  //   })
-  // }})
+//   // ERROR: (only if resume is defined as capturing, is this right?)
+//   // val badHandler: FnProducer = Handler({_ => { resume => 
+//   //   println("Unsafe handler invoked!")
+//   //   resume(() => {
+//   //     resume(() => ())
+//   //   })
+//   // }})
 
-  // ERROR: Resume captures resume
-  // val badHandler: FnProducer = new Handler {
-  //   override def handle(x: Unit, resume: (() -> Unit) => Unit): Unit = {
-  //     println("Unsafe handler invoked!")
-  //     resume(() => {
-  //       resume(() => ())
-  //     })
-  //   }
-  // }
-  val result: Unit = run(goodHandler)(program(5))
-}
+//   // ERROR: Resume captures resume
+//   // val badHandler: FnProducer = new Handler {
+//   //   override def handle(x: Unit, resume: (() -> Unit) => Unit): Unit = {
+//   //     println("Unsafe handler invoked!")
+//   //     resume(() => {
+//   //       resume(() => ())
+//   //     })
+//   //   }
+//   // }
+//   val result: Unit = run(goodHandler)(program(5))
+// }
 
-lazy object SmuggledHandlerDemo {
-  type IntProducer = Handler[Unit, Int, Unit, Unit]
+// lazy object SmuggledHandlerDemo {
+//   type IntProducer = Handler[Unit, Int, Unit, Unit]
 
-  var handlerStorage: Option[IntProducer^] = None
+//   var handlerStorage: Option[IntProducer^] = None
 
-  val goodHandler: IntProducer^ = Handler({_ => { resume => resume(5)}})
+//   val goodHandler: IntProducer^ = Handler({_ => { resume => resume(5)}})
 
-  val naughtyProgram: IntProducer ?-> Unit = {
-    val h: IntProducer^ = summon[IntProducer^]
-    // handlerStorage = Some(h) // ERROR: Leak the handler itself
-  }
+//   val naughtyProgram: IntProducer ?-> Unit = {
+//     val h: IntProducer^ = summon[IntProducer^]
+//     // handlerStorage = Some(h) // ERROR: Leak the handler itself
+//   }
 
-  val result = run(goodHandler)(naughtyProgram)
-  // val smuggledHandler: IntProducer = handlerStorage.get // Use the handler
-}
+//   val result = run(goodHandler)(naughtyProgram)
+//   // val smuggledHandler: IntProducer = handlerStorage.get // Use the handler
+// }
 
-lazy object SmuggledHandlerFnDemo {
-  type IntProducer = Handler[Unit, Int, Unit, Unit]
+// lazy object SmuggledHandlerFnDemo {
+//   type IntProducer = Handler[Unit, Int, Unit, Unit]
 
-  var fnStorage: Option[() => Unit] = None
+//   var fnStorage: Option[() => Unit] = None
 
-  val goodHandler: IntProducer = Handler({_ => { resume => resume(5)}})
+//   val goodHandler: IntProducer = Handler({_ => { resume => resume(5)}})
 
-  val naughtyProgram: IntProducer ?-> Unit = {
-    val h = summon[IntProducer]
-    // fnStorage = Some(() => h.handle((), _ => ())) // ERROR: Leak the handle function
-  }
+//   val naughtyProgram: IntProducer ?-> Unit = {
+//     val h = summon[IntProducer]
+//     // fnStorage = Some(() => h.handle((), _ => ())) // ERROR: Leak the handle function
+//   }
 
-  val result = run(goodHandler)(naughtyProgram)
-  // val smuggledFn = fnStorage.get
-  // smuggledFn()
-}
+//   val result = run(goodHandler)(naughtyProgram)
+//   // val smuggledFn = fnStorage.get
+//   // smuggledFn()
+// }
