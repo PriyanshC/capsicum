5e04463e7f9dc79a66a0e7d838b6f86bc2493312
Priyansh <priyanshchugh@outlook.com>
2026-04-08 18:42:18 +0100

    refactor state handler into abstract class and concrete impl

--- src/main/scala/capsicum/Capsicum.scala
+++ src/main/scala/capsicum/Capsicum.scala
@@ -4,39 +4,36 @@ import language.experimental.captureChecking
 
 trait Effect[+V]
 
-abstract class Handler[E[+_], P, R]() extends caps.ExclusiveCapability {
-  def handle[V](op: E[V], resume: => V => P): R
+abstract class Capability[E[+_], P, R]() extends caps.ExclusiveCapability {
+  def perform[V](op: E[V], resume: => V => P): R
 }
 
-object Handler {
-  def apply[E[+_], P, R, C^](f: ([V] => (E[V], (V => P)) => R)^{C}): Handler[E, P, R]^{C} =
-      new Handler[E, P, R] {
-        override def handle[V](op: E[V], resume: => V => P): R = f[V](op, resume)
+object Capability {
+  def apply[E[+_], P, R, C^](f: ([V] => (E[V], (V => P)) => R)^{C}): Capability[E, P, R]^{C} =
+      new Capability[E, P, R] {
+        override def perform[V](op: E[V], resume: => V => P): R = f[V](op, resume)
       }
 }
 
 // note to self: check against `prog ?-> P`
-def run[E[+_], P, R](handler: Handler[E, P, R]^)(prog: (Handler[E, P, R]^{handler}) ?-> R): R = prog(using handler)
+def run[E[+_], P, R](handler: Capability[E, P, R]^)(prog: (Capability[E, P, R]^{handler}) ?-> R): R = prog(using handler)
 
-object State {
-  enum StateOp[S, +R] extends Effect[R] {
-    case Get[S]() extends StateOp[S, S]
-    case Put[S](value: S) extends StateOp[S, Unit]
-  }
 
-  def newState[S, R](initial: S): Handler[[V] =>> StateOp[S, V], R, R] = {
-    var state: S = initial
+enum StateOp[S, +R] extends Effect[R] {
+  case Get[S]() extends StateOp[S, S]
+  case Put[S](value: S) extends StateOp[S, Unit]
+}
 
-    new Handler[[V] =>> StateOp[S, V], R, R] {
-      override def handle[V](op: StateOp[S, V], resume: => V => R): R = 
-        op match {
-          case StateOp.Get() => 
-            resume(state.asInstanceOf[V])
-            
-          case StateOp.Put(newValue) => 
-            state = newValue.asInstanceOf[S]
-            resume(().asInstanceOf[V])
-        }
+abstract class StateCapability[S, R] extends Capability[[V] =>> StateOp[S, V], R, R] {
+  final def get(resume: => S => R): R = perform(StateOp.Get(), resume)
+  final def put(newState: S, resume: => Unit => R): R = perform(StateOp.Put(newState), resume)
+}
+
+class StateHandler[S, R](private var state: S) extends StateCapability[S, R] {
+  override def perform[V](op: StateOp[S, V], resume: => V => R): R = op match
+    case StateOp.Get() => resume(state)
+    case StateOp.Put(newState) => {
+      state = newState
+      resume(())
     }
-  }
 }
