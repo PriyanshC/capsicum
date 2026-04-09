75735ede85019e7f320216d8a098567f4c5290ca
Priyansh <priyanshchugh@outlook.com>
2026-04-09 13:22:29 +0100

    trait over abstract class and fix shadowed generic2

--- src/main/scala/capsicum/Capsicum.scala
+++ src/main/scala/capsicum/Capsicum.scala
@@ -4,7 +4,7 @@ import language.experimental.captureChecking
 
 trait Effect[+V]
 
-abstract class Capability[E[+_], P, R]() extends caps.ExclusiveCapability {
+trait Capability[E[+V] <: Effect[V], P, R]() extends caps.ExclusiveCapability {
   def perform[V](op: E[V], resume: => V => P): R
 }
 
@@ -16,15 +16,15 @@ object Capability {
 }
 
 // note to self: check against `prog ?-> P`
-def run[E[+_], P, R](handler: Capability[E, P, R]^)(prog: (Capability[E, P, R]^{handler}) ?-> R): R = prog(using handler)
+def run[E[+V] <: Effect[V], P, R](handler: Capability[E, P, R]^)(prog: (Capability[E, P, R]^{handler}) ?-> R): R = prog(using handler)
 
 
 enum StateOp[S, +R] extends Effect[R] {
-  case Get[S]() extends StateOp[S, S]
-  case Put[S](value: S) extends StateOp[S, Unit]
+  case Get() extends StateOp[S, S]
+  case Put(value: S) extends StateOp[S, Unit]
 }
 
-abstract class StateCapability[S, R] extends Capability[[V] =>> StateOp[S, V], R, R] {
+trait StateCapability[S, R] extends Capability[[X] =>> StateOp[S, X], R, R] {
   final def get(resume: => S => R): R = perform(StateOp.Get(), resume)
   final def put(newState: S, resume: => Unit => R): R = perform(StateOp.Put(newState), resume)
 }
