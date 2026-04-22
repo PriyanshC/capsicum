f1c3f1fc9ef2cb4b2b150114acc2e7fe60e15df5
Priyansh <priyanshchugh@outlook.com>
2026-04-22 19:23:14 +0100

    basecap and symmcap

--- src/main/scala/capsicum/Capsicum.scala
+++ src/main/scala/capsicum/Capsicum.scala
@@ -4,9 +4,11 @@ import language.experimental.captureChecking
 
 trait Effect { type Result }
 
-trait Capability[E <: Effect, P, R]() extends caps.ExclusiveCapability {
+sealed trait BaseCapability[E <: Effect, P, R]() {
   def perform(eff: E, resume: eff.Result => P): R
 }
+
+trait Capability[E <: Effect, P, R]() extends BaseCapability[E, P, R] with caps.ExclusiveCapability
 
 object Capability {
   def apply[E <: Effect, P, R, C^](f: ([V] => (E, (V => P)) => R)^{C}): Capability[E, P, R]^{C} = new Capability[E, P, R] {
@@ -18,6 +20,15 @@ object Capability {
 // note to self: find an example explicitly prohibited by prog^{handler}
 def run[E <: Effect, P, R](handler: Capability[E, P, R]^)(prog: (Capability[E, P, R]^{handler}) ?-> R): R = prog(using handler)
 
+
+type UniformCapability[E <: Effect, R] = Capability[E, R, R]
+
+trait TailResumptiveCap[E <: Effect, R] extends UniformCapability[E, R] {
+  def eval(eff: E): eff.Result
+  
+  // inline?
+  final def perform(eff: E, resume: eff.Result => R): R = resume(eval(eff))
+}
 
 sealed trait StateEff[S] extends Effect
 
