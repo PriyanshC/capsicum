063fae4f185c330114befb984baf6da348d2af98
Priyansh <priyanshchugh@outlook.com>
2026-04-22 19:56:29 +0100

    IO cap

---
+++ src/main/scala/capsicum/Console.scala
@@ -0,0 +1,27 @@
+package capsicum
+
+import language.experimental.captureChecking
+
+sealed trait ConsoleEff extends Effect
+
+object ConsoleOp {
+  case class Print(s: String) extends ConsoleEff { type Result = Unit }
+  case class ReadLine() extends ConsoleEff { type Result = String }
+}
+
+trait ConsoleCapability[R] extends UniformCapability[ConsoleEff, R] {
+  final def print(s: String, resume: Unit => R): R = perform(ConsoleOp.Print(s), resume)
+  final def readLine(resume: String => R): R = perform(ConsoleOp.ReadLine(), resume)
+}
+
+class StdConsoleHandler[R] extends ConsoleCapability[R] {
+    override def perform(eff: ConsoleEff, resume: eff.Result => R): R = eff match
+        case ConsoleOp.Print(s) => {
+            scala.Console.print(s)
+            resume.asInstanceOf[Unit => R](())
+        }
+        case ConsoleOp.ReadLine() => {
+            val s = scala.io.StdIn.readLine()
+            resume.asInstanceOf[String => R](s)
+        }    
+}
