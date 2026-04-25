51509399d2e9257f4e1b30146c163440eb66dac7
Priyansh <priyanshchugh@outlook.com>
2026-04-25 12:16:12 +0100

    concurrency example

---
+++ src/main/scala/capsicum/examples/Concurrency.scala
@@ -0,0 +1,25 @@
+package capsicum.examples
+
+import capsicum.core._
+import capsicum.effects._
+import language.experimental.captureChecking
+
+def basicAsyncProgram(): Unit = {
+  
+  def prog(using async: AsyncCapability[Unit]): Unit = {
+    async.fork(() => {
+      println("Hello from virtual thread!")
+      42
+    }, fiber => {
+      println("Forked successfully...")
+      async.join(fiber, result => {
+        println(s"Got result: $result")
+      })
+    })
+  }
+  
+  val handler = new LoomAsyncHandler[Unit]()
+  run[AsyncEff, AsyncCapability[Unit], Unit, Unit](handler) {
+    prog(using summon[AsyncCapability[Unit]])
+  }
+}
