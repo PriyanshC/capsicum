c46651a3947f09725ea556455f709c3c5387d6f2
Priyansh <priyanshchugh@outlook.com>
2026-04-27 15:50:33 +0100

    Raise updated

--- src/main/scala/capsicum/effects/Raise.scala
+++ src/main/scala/capsicum/effects/Raise.scala
@@ -3,13 +3,18 @@ package capsicum.effects
 import capsicum.core._
 import language.experimental.captureChecking
 
-case class RaiseEff[Err](e: Err) extends Effect { type Result = Nothing }
+case class RaiseEff[Err, V](e: Err) extends Effect[V]
 
-trait RaiseCapability[Err, A, R] extends Capability[RaiseEff[Err], A, R] {
-  final def raise(err: Err): R = perform(RaiseEff(err), _ => sys.error("unreachable"))
+trait RaiseCapability[Err, P, R] extends ExclusiveCapability[[V] =>> RaiseEff[Err, V], P, R] {
+  final def raise(err: Err, resume: Err => P): R = perform(RaiseEff(err), resume)
 }
 
-class EitherExcHandler[Err, A] extends RaiseCapability[Err, A, Either[Err, A]] {
-  override def perform(eff: RaiseEff[Err], resume: eff.Result => A): Either[Err, A] = eff match
-    case RaiseEff(err) => Left(err)
-}
+
+// class EitherExcHandler[Err, A] extends RaiseCapability[Err, A] {
+//   def perform[V](eff: E[V], resume: V => P): R = ???
+// }
+
+// class EitherExcHandler[Err, A] extends RaiseCapability[[V] =>> RaiseEff[Err], A, Either[Err, A]] {
+//   override def perform(eff: RaiseEff[Err], resume: Err => A): Either[Err, A] = eff match
+//     case RaiseEff(err) => Left(err)
+// }
