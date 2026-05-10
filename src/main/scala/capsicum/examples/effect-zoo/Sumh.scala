c899eec86c192f8ed4a5784ba5f9ad25cbc63934
Priyansh <priyanshchugh@outlook.com>
2026-05-10 17:23:00 +0100

    tmp remove sumh

--- src/main/scala/capsicum/examples/effect-zoo/Sumh.scala
+++ src/main/scala/capsicum/examples/effect-zoo/Sumh.scala
@@ -1,29 +1,29 @@
-package capsicum.examples
+// package capsicum.examples
 
-import capsicum.core._
-import capsicum.effects._
-import language.experimental.captureChecking
+// import capsicum.core._
+// import capsicum.effects._
+// import language.experimental.captureChecking
 
-object Sumh {
-  def LIMIT = 1000
+// object Sumh {
+//   def LIMIT = 1000
 
-  inline def program(r: Int)(using count: StateCapability[Int, Bounce[(Int, Long)]], sum: StateCapability[Long, Bounce[(Int, Long)]]): Bounce[(Int, Long)] = {
-    def rec: Bounce[(Int, Long)] = {
-      count.get { s =>
-        count.update(_ + 1, { _ =>
-          sum.update(_ + s.toLong, {_ =>
-            if s < r then suspend(rec) else count.get(c => result((c, s)))
-          })
-        })
-      }
-    }
-    rec
-  }
+//   inline def program(r: Int)(using count: StateCapability[Int, Bounce[(Int, Long)]], sum: StateCapability[Long, Bounce[(Int, Long)]]): Bounce[(Int, Long)] = {
+//     def rec: Bounce[(Int, Long)] = {
+//       count.get { s =>
+//         count.update(_ + 1, { _ =>
+//           sum.update(_ + s.toLong, {_ =>
+//             if s < r then suspend(rec) else count.get(c => result((c, s)))
+//           })
+//         })
+//       }
+//     }
+//     rec
+//   }
 
-  def round1 = {
-    val state = new MutableStateHandler[Int, Bounce[(Int, Long)]](0)
-    val sum = new MutableStateHandler[Long, Bounce[(Int, Long)]](0L)
-    val (resInt, resLong) = run(state, sum)(program(Sumh.LIMIT)).eval
-    (resInt, resLong, resInt + 1)
-  }
-}
+//   def round1 = {
+//     val state = new MutableStateHandler[Int, Bounce[(Int, Long)]](0)
+//     val sum = new MutableStateHandler[Long, Bounce[(Int, Long)]](0L)
+//     val (resInt, resLong) = run(state, sum)(program(Sumh.LIMIT)).eval
+//     (resInt, resLong, resInt + 1)
+//   }
+// }
