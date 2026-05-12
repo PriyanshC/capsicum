e8f664349b5996b5d61332795a363c8644b04315
Priyansh <priyanshchugh@outlook.com>
2026-05-12 14:40:00 +0100

    Uncomment mulst

--- src/main/scala/capsicum/examples/effect-zoo/Mulst.scala
+++ src/main/scala/capsicum/examples/effect-zoo/Mulst.scala
@@ -1,7 +1,12 @@
 package capsicum.examples
+package capsicum.examples
 
 import capsicum.core._
 import capsicum.effects._
+import capsicum.core._
+import capsicum.effects._
+
+import language.experimental.captureChecking
 
 object Mulst {
   def LIMIT = 100
@@ -9,7 +14,7 @@ object Mulst {
   def round1 = {
     type Ret = Bounce[Int]
     type St = StateCapability[Int, Ret]
-    def prog(n: Int)(using s: St): Ret = {
+    inline def prog(n: Int)(using s: St): Ret = {
       def rec(n: Int): Ret = {
         if n <= 0 then s.get(x => result(x)) else {
           s.update(_ + 1, { _ =>
@@ -29,11 +34,14 @@ object Mulst {
     val s = new MutableStateHandler[Int, Ret](0)
     s.run(prog(Mulst.LIMIT)).eval
   }
+    val s = new MutableStateHandler[Int, Ret](0)
+    s.run(prog(Mulst.LIMIT)).eval
+  }
 
   def round2 = {
     type Ret = Bounce[(Int, Int)]
     type St = StateCapability[Int, Ret]
-    def prog(n: Int)(using s1: St, s2: St): Ret = {
+    inline def prog(n: Int)(using s1: St, s2: St): Ret = {
       def rec(n: Int): Ret = {
         if n <= 0 then s1.get(x1 => s2.get(x2 => result(x1, x2))) else {
           s1.update(_ + 1, { _ =>
@@ -54,11 +62,15 @@ object Mulst {
     val s2 = new MutableStateHandler[Int, Ret](0)
     prog(Mulst.LIMIT)(using s1, s2).eval
   }
+    val s1 = new MutableStateHandler[Int, Ret](0)
+    val s2 = new MutableStateHandler[Int, Ret](0)
+    prog(Mulst.LIMIT)(using s1, s2).eval
+  }
 
   def round3 = {
     type Ret = Bounce[(Int, Int, Int)]
     type St = StateCapability[Int, Ret]
-    def prog(n: Int)(using s1: St, s2: St, s3: St): Ret = {
+    inline def prog(n: Int)(using s1: St, s2: St, s3: St): Ret = {
       def rec(n: Int): Ret= {
         if n <= 0 then s1.get(x1 => s2.get(x2 => s3.get(x3 => result((x1, x2, x3))))) else {
           s1.update(_ + 1, { _ =>
@@ -83,7 +95,7 @@ object Mulst {
   def round4 = {
     type Ret = Bounce[(Int, Int, Int, Int)]
     type St = StateCapability[Int, Ret]
-    def prog(n: Int)(using s1: St, s2: St, s3: St, s4: St): Ret = {
+    inline def prog(n: Int)(using s1: St, s2: St, s3: St, s4: St): Ret = {
       def rec(n: Int): Ret= {
         if n <= 0 then s1.get(x1 => s2.get(x2 => s3.get(x3 => s4.get(x4 => result((x1, x2, x3, x4)))))) else {
           s1.update(_ + 1, { _ =>
@@ -109,7 +121,7 @@ object Mulst {
   def round5 = {
     type Ret = Bounce[(Int, Int, Int, Int, Int)]
     type St = StateCapability[Int, Ret]
-    def prog(n: Int)(using s1: St, s2: St, s3: St, s4: St, s5: St): Ret = {
+    inline def prog(n: Int)(using s1: St, s2: St, s3: St, s4: St, s5: St): Ret = {
       def rec(n: Int): Ret= {
         if n <= 0 then s1.get(x1 => s2.get(x2 => s3.get(x3 => s4.get(x4 => s5.get(x5 => result((x1, x2, x3, x4, x5))))))) else {
           s1.update(_ + 1, { _ =>
