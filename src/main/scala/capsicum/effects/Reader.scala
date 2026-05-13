52e0c7c54d8b9a3d9d2eccefd44c39b4754b2270
Priyansh <priyanshchugh@outlook.com>
2026-05-13 14:36:32 +0100

    FIx inlining and made env cap

--- src/main/scala/capsicum/effects/Reader.scala
+++ src/main/scala/capsicum/effects/Reader.scala
@@ -9,3 +9,8 @@ case class Ask[T]() extends Reader[T, T]
 trait ReaderCapability[T, P, R] extends Capability[[V] =>> Reader[T, V], P, R] {
   final inline def ask(inline resume: T => P): R = perform(Ask(), resume)
 }
+
+class EnvCapability[T, R](env: T) extends ReaderCapability[T, R, R] with DirectCap[[V] =>> Reader[T, V], R]{
+  override protected def apply[V](eff: Reader[T, V]): V = eff match
+    case Ask() => env
+}
--- src/main/scala/capsicum/effects/Writer.scala
+++ src/main/scala/capsicum/effects/Writer.scala
@@ -7,5 +7,5 @@ trait Writer[T, V] extends Effect[V]
 case class Tell[T](t: T) extends Writer[T, T]
 
 trait WriterCapability[T, P, R] extends Capability[[V] =>> Writer[T, V], P, R] {
-  final inline def tell(inline t: T, inline resume: T => P): R = perform(Tell(t), resume)
+  final inline def tell(t: T, inline resume: T => P): R = perform(Tell(t), resume)
 }
