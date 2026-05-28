f55c4298139377f7d31fd4c6bd9851d94f0a3f8d
Priyansh <priyanshchugh@outlook.com>
2026-05-28 14:27:03 +0100

    COmment

--- experiments/src/main/scala/motivating-examples/cc-on/Async.scala
+++ experiments/src/main/scala/motivating-examples/cc-on/Async.scala
@@ -12,24 +12,24 @@ import scala.language.experimental.captureChecking
 object KyoAsyncExCC extends KyoApp {
   import kyo._
   run {
-    val prog: String < ((Async^) & Env[Reader[String]]) = {
-      for {
-        _ <- Async.sleep(2.seconds)
-        // env <- Env.get[Reader[String]]
-        traceId = ""//env.ask()
-      } yield s"Processed with id=$traceId"
-    }
+    // val prog: String < ((Async^) & Env[Reader[String]]) = {
+    //   for {
+    //     _ <- Async.sleep(2.seconds)
+    //     // env <- Env.get[Reader[String]]
+    //     traceId = ""//env.ask()
+    //   } yield s"Processed with id=$traceId"
+    // }
 
 
-    val handledEnv: String < Async = Reader.run("ID-5") { env ?=>
-      Env.run(env)(prog)
-    }
+    // val handledEnv: String < Async = Reader.run("ID-5") { env ?=>
+    //   Env.run(env)(prog)
+    // }
     
 
-    Abort.run(Async.timeout(5.seconds)(handledEnv)).map {
-      case Result.Success(res) => println(s"Success: $res")
-      case Result.Panic(ex)    => println(s"Task failed with: ${ex.getMessage}")
-      case Result.Failure(ex)  => println(s"Task failed with: ${ex.getMessage}")
-    }
+    // Abort.run(Async.timeout(5.seconds)(handledEnv)).map {
+    //   case Result.Success(res) => println(s"Success: $res")
+    //   case Result.Panic(ex)    => println(s"Task failed with: ${ex.getMessage}")
+    //   case Result.Failure(ex)  => println(s"Task failed with: ${ex.getMessage}")
+    // }
   }
 }
