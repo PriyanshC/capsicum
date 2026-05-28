0da669ce3d75c9840db8e139ad3b429923b5c975
Priyansh <priyanshchugh@outlook.com>
2026-05-28 17:08:33 +0100

    Refine mut state

--- src/test/scala/capsicum/effects/TestState.scala
+++ src/test/scala/capsicum/effects/TestState.scala
@@ -5,24 +5,27 @@ import org.scalacheck.Prop.forAll
 import scala.annotation.init
 
 abstract class StateLaws[S: Arbitrary](
-  newState: => StateCapability[S, Boolean]
+  newState: S => StateCapability[S, Boolean]
 ) extends Properties(s"StateLaws for ${newState.getClass.getSimpleName()}") {
 
-  property("Put-Get") = forAll { (value: S) =>
-    val state = newState
+  property("Get") = forAll { (initial: S) =>
+    val state = newState(initial)
+    state.get(_ == initial)
+  }
 
+  property("Put-Get") = forAll { (initial: S, value: S) =>
+    val state = newState(initial)
     state.put(value, _ => state.get(_ == value))
   }
 
-  property("Put-Put-Get") = forAll { (val1: S, val2: S) =>
-    val state = newState
-    
+  property("Put-Put-Get") = forAll { (initial: S, val1: S, val2: S) =>
+    val state = newState(initial)
     state.put(val1, _ => state.put(val2, _ => state.get(_ == val2)))
   }
 }
 
-abstract class MutStateLaws[S: Arbitrary](initial: S) extends StateLaws[S](new MutableStateHandler(initial))
+abstract class MutStateLaws[S: Arbitrary] extends StateLaws[S](new MutableStateHandler(_))
 
-object MutIntStateSpec extends MutStateLaws[Int]((0))
-object MutStringStateSpec extends MutStateLaws[String]((""))
-object MutListStateSpec extends MutStateLaws[List[Double]]((Nil))
+object MutIntStateSpec extends MutStateLaws[Int]
+object MutStringStateSpec extends MutStateLaws[String]
+object MutListStateSpec extends MutStateLaws[List[Double]]
