2c4513dc7429a2c2fde1a2e55cd4b229970a545d
Priyansh <priyanshchugh@outlook.com>
2026-05-28 15:09:06 +0100

    TURBYLIFT async

--- experiments/src/main/scala/motivating-examples/cc-on/Async.scala
+++ experiments/src/main/scala/motivating-examples/cc-on/Async.scala
@@ -5,8 +5,27 @@ import scala.concurrent.duration._
 import scala.util.Try
 import scala.concurrent.ExecutionContext.Implicits.global
 import kyo.KyoApp
+import experiments.motivation.ccoff.async.Reader
 
 import scala.language.experimental.captureChecking
+
+trait TrackedReader[S] extends Reader[S] with caps.SharedCapability
+
+object Reader {
+  private val threadContext = new ThreadLocal[String]
+
+  def runTracked[A](env: String)(prog: TrackedReader[String] ?=> A): A = {
+    threadContext.set(env)
+      val capability = new TrackedReader[String] {
+        def ask(): String = threadContext.get()
+      }
+      
+      val result = prog(using capability)
+      threadContext.remove()
+      result
+  }
+}
+
 
 
 object KyoCc extends KyoApp {
@@ -31,5 +50,32 @@ object KyoCc extends KyoApp {
     //   case Result.Panic(ex)    => println(s"Task failed with: ${ex.getMessage}")
     //   case Result.Failure(ex)  => println(s"Task failed with: ${ex.getMessage}")
     // }
+  }
+}
+
+object Turbolift {
+  import turbolift._
+  import turbolift.effects.{ReaderEffect, IO}
+
+  case object TraceEnv extends ReaderEffect[TrackedReader[String]]
+
+  @main def runTurboliftApp() = {
+    val prog = for {
+      _   <- IO.sleep(2.seconds)
+      // env <- TraceEnv.ask
+      // traceId = ""//env.ask()
+    }
+    // yield s"Processed with id=$traceId"
+    yield ""
+
+
+    // val handledEnv = Reader.runTracked("ID-5") { env ?=>
+    //   prog.handleWith(TraceEnv.handler(env))
+    // }
+
+    // val finalProg = IO.timeout(handledEnv, 5.seconds)
+
+    // val result = finalProg.runIO
+    // println(result)
   }
 }
