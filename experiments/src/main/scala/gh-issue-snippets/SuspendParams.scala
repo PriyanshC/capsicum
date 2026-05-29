d58d9e060f3217a3c9beb62a91856972275e6725
Priyansh <priyanshchugh@outlook.com>
2026-05-29 14:41:43 +0100

    minifying suspendparam

--- experiments/src/main/scala/gh-issue-snippets/SuspendParams.scala
+++ experiments/src/main/scala/gh-issue-snippets/SuspendParams.scala
@@ -3,38 +3,22 @@ package experiments.issues.suspendparams
 import language.experimental.captureChecking
 import scala.annotation.tailrec
 
-// Bounce
-sealed abstract class Bounce[A] {
-    final def eval: A = this match {
-    case thunk: Thunk[A] => thunk.cont().eval
-    case chunk: Chunk[A] => chunk.x
-  }
-}
+class Bounce[A]
 
-def suspend[A, C^, D^](x: ->{C} Bounce[A]^{D}): Bounce[A]^{C, D} = Thunk(() => x)
-inline def result[A](x: A): Bounce[A] = Chunk(x)
-
-private final class Chunk[A](val x: A) extends Bounce[A]
-private final class Thunk[A](val cont: () => Bounce[A]^) extends Bounce[A]
+def suspend[A, C^, D^](x: ->{C} Bounce[A]^{D}): Bounce[A]^{C, D} = new Bounce[A]
+inline def result[A](x: A): Bounce[A] = new Bounce[A]
 
 
 // Cap
 trait Effect[V]
 sealed trait BaseCapability[-E <: Effect, -P, R] {
   def perform[V](eff: E[V], resume: V => P): R^{resume}
-  final inline def run(inline prog: this.type ?=> R): R = prog(using this)
 }
-
-trait Capability[-E <: Effect, -P, R] extends BaseCapability[E, P, R] with caps.SharedCapability
 
 def run[K1 <: BaseCapability[?, ?, R], K2 <: BaseCapability[?, ?, R], R](
 k1: K1, k2: K2
 )(prog: (K1, K2) ?-> R): R = {
-  k1.run {
-    k2.run {
-      prog(using k1, k2)
-    }
-  }
+  prog(using k1, k2)
 }
 
 // State
@@ -47,7 +31,7 @@ object StateOp {
   case class Put[S](value: S) extends StateEff[S, Unit]
 }
 
-trait StateCapability[S, R] extends Capability[State[S], R, R] {
+trait StateCapability[S, R] extends BaseCapability[State[S], R, R] with caps.SharedCapability {
   final inline def get(inline resume: S => R): R = perform(StateOp.Get(), resume)
   final inline def put(inline newState: S, inline resume: Unit => R): R = perform(StateOp.Put(newState), resume)
   final inline def update(inline upd: S => S, inline resume: Unit => R): R = get(s => put(upd(s), resume))
@@ -61,7 +45,7 @@ class MutableStateHandler[S, R](var state: S) extends StateCapability[S, R] with
 }
 
 trait DirectCap[-E <: Effect, R] {
-  this: Capability[E, R, R]^ =>
+  this: BaseCapability[E, R, R]^ =>
     protected def apply[V](eff: E[V]): V
     final override def perform[V](eff: E[V], resume: V => R): R = resume(apply(eff))
 }
@@ -69,15 +53,13 @@ trait DirectCap[-E <: Effect, R] {
 // Prog
 
 object Sumh {
-  def LIMIT = 1000
-
-  inline def program(r: Int)(using count: StateCapability[Int, Bounce[(Int, Long)]], sum: StateCapability[Long, Bounce[(Int, Long)]]): Bounce[(Int, Long)] = {
+  inline def program(using count: StateCapability[Int, Bounce[(Int, Long)]], sum: StateCapability[Long, Bounce[(Int, Long)]]): Bounce[(Int, Long)] = {
     def rec: Bounce[(Int, Long)] = {
       count.get { s =>
         count.update(_ + 1, { _ =>
           sum.update(_ + s.toLong, {_ =>
             // if s < r then suspend[(Int, Long), {}, {}](rec) else count.get(c => result((c, s)))
-            if s < r then suspend(rec) else count.get(c => result((c, s)))
+            if s < 100 then suspend(rec) else count.get(c => result((c, s)))
           })
         })
       }
@@ -88,7 +70,6 @@ object Sumh {
   def round1 = {
     val state = new MutableStateHandler[Int, Bounce[(Int, Long)]](0)
     val sum = new MutableStateHandler[Long, Bounce[(Int, Long)]](0L)
-    val (resInt, resLong) = run(state, sum)(program(100)).eval
-    (resInt, resLong, resInt + 1)
+    val bounce = run(state, sum)(program)
   }
 }
--- src/main/scala/capsicum/examples/effect-zoo/Cdown.scala
+++ src/main/scala/capsicum/examples/effect-zoo/Cdown.scala
@@ -1,44 +1,44 @@
-package capsicum.examples
+// package capsicum.examples
 
-import capsicum.core._
-import capsicum.effects._
-import language.experimental.captureChecking
+// import capsicum.core._
+// import capsicum.effects._
+// import language.experimental.captureChecking
 
 
-object Cdown {
-  def LIMIT = 10000
-}
+// object Cdown {
+//   def LIMIT = 10000
+// }
 
-object MutableEntry {
-  inline def program(using state: StateCapability[Int, Bounce[Int]]) = {
-    def rec: Bounce[Int] = {
-      state.get { s =>
-        if (s <= 0) then result(s) else state.put(s - 1, _ => suspend(rec))
-      }
-    }
-    rec
-  }
+// object MutableEntry {
+//   inline def program(using state: StateCapability[Int, Bounce[Int]]) = {
+//     def rec: Bounce[Int] = {
+//       state.get { s =>
+//         if (s <= 0) then result(s) else state.put(s - 1, _ => suspend(rec))
+//       }
+//     }
+//     rec
+//   }
 
-  def round1 = {
-    State.runMutSafe(Cdown.LIMIT)(program)
-  }
-}
+//   def round1 = {
+//     State.runMutSafe(Cdown.LIMIT)(program)
+//   }
+// }
 
-object PureEntry {
-  inline def program(using state: SafePureStateCapability[Int, Int]): Int -> Bounce[(Int, Int)] = {
-    def rec: Int -> Bounce[(Int, Int)] = {
-      state.get { s =>
-        if (s <= 0) then 
-          ((x: Int) => result((x, s))) 
-        else 
-          state.put(s - 1, _ => rec)
-      }
-    }
-    rec
-  }
+// object PureEntry {
+//   inline def program(using state: SafePureStateCapability[Int, Int]): Int -> Bounce[(Int, Int)] = {
+//     def rec: Int -> Bounce[(Int, Int)] = {
+//       state.get { s =>
+//         if (s <= 0) then 
+//           ((x: Int) => result((x, s))) 
+//         else 
+//           state.put(s - 1, _ => rec)
+//       }
+//     }
+//     rec
+//   }
 
-  def round1 = {
-    val (finalState, res) = State.runPureSafe(10000)(program)
-    res
-  }
-}
+//   def round1 = {
+//     val (finalState, res) = State.runPureSafe(10000)(program)
+//     res
+//   }
+// }
