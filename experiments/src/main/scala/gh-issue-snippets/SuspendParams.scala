2add6784c775153f19bf6615f9852673ef1aa370
Priyansh <priyanshchugh@outlook.com>
2026-05-29 17:39:18 +0100

    Minify away state eff

--- experiments/src/main/scala/gh-issue-snippets/SuspendParams.scala
+++ experiments/src/main/scala/gh-issue-snippets/SuspendParams.scala
@@ -4,25 +4,22 @@ import language.experimental.captureChecking
 
 // State
 sealed trait State[S, R] {
-  def perform[V](eff: StateOp[S, V], resume: V => R): R^{resume}
-  final inline def get(inline resume: S => R): R = perform(StateOp.Get(), resume)
-  final inline def put(inline newState: S, inline resume: Unit => R): R = perform(StateOp.Put(newState), resume)
-  final inline def update(inline upd: S => S, inline resume: Unit => R): R = get(s => put(upd(s), resume))
+  def get(resume: S => R): R^{resume}
+  def put(newState: S, resume: Unit => R): R^{resume}
 }
 
 def run[S1 <: State[?, R], S2 <: State[?, R], R](s1: S1, s2: S2)(prog: (S1, S2) ?-> R): R = prog(using s1, s2)
 
-sealed trait StateOp[S, V]
-object StateOp {
-  case class Get[S]() extends StateOp[S, S]
-  case class Put[S](v: S) extends StateOp[S, Unit]
-}
-
 // Prog
 inline def program(using sum: State[Long, (Int, Long)]): (Int, Long) = {
-  def rec(s: Int): (Int, Long) = sum.update(_ + s, {_ =>
-    if s > 0 then rec(s - 1) else (0, 0)
-  })
+  def rec(x: Int): (Int, Long) = {
+    sum.get { s0 =>
+      val s = s0 + x
+      sum.put(s, {_ =>
+        if x > 0 then rec(x - 1) else (x, s)
+      })
+    }
+  }
   rec(10)
 }
 
