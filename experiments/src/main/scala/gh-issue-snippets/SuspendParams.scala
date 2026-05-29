9d31a05057b13659549ee3f229b5da7da346fae9
Priyansh <priyanshchugh@outlook.com>
2026-05-29 18:00:40 +0100

    Remove context functions

--- experiments/src/main/scala/gh-issue-snippets/SuspendParams.scala
+++ experiments/src/main/scala/gh-issue-snippets/SuspendParams.scala
@@ -7,9 +7,9 @@ trait State[S, R] {
   def put(newState: S, resume: Unit => R): R^{resume}
 }
 
-def run2[S1 <: State[?, R], S2 <: State[?, R], R](s1: S1, s2: S2)(prog: (S1, S2) ?=> R): R = prog(using s1, s2)
+def run2[S1 <: State[?, R], S2 <: State[?, R], R](s1: S1, s2: S2)(prog: (S1, S2) => R): R = prog(s1, s2)
 
-inline def program(using sum: State[Long, (Int, Long)]): (Int, Long) = {
+inline def program(sum: State[Long, (Int, Long)]): (Int, Long) = {
   def rec(x: Int): (Int, Long) = {
     sum.get { s0 =>
       val s = s0 + x
@@ -22,5 +22,5 @@ inline def program(using sum: State[Long, (Int, Long)]): (Int, Long) = {
 }
 
 def runProgram(other: State[?, (Int, Long)], sum: State[Long, (Int, Long)]) = {
-  run2(other, sum)((_, s) ?=> program(using s))
+  run2(other, sum)((_, s) => program(s))
 }
