c52dd0a25db058a67c16e578a08d466954ea7757
Priyansh <priyanshchugh@outlook.com>
2026-05-29 15:06:08 +0100

    Minify statecap

--- experiments/src/main/scala/gh-issue-snippets/SuspendParams.scala
+++ experiments/src/main/scala/gh-issue-snippets/SuspendParams.scala
@@ -8,41 +8,33 @@ class Bounce[A]
 def suspend[A, C^, D^](x: ->{C} Bounce[A]^{D}): Bounce[A]^{C, D} = ???
 inline def result[A](x: A): Bounce[A] = ???
 
-
 // Cap
 sealed trait State[S, R] {
-  def perform[V](eff: StateEff[S, V], resume: V => R): R^{resume}
+  def perform[V](eff: StateOp[S, V], resume: V => R): R^{resume}
   final inline def get(inline resume: S => R): R = perform(StateOp.Get(), resume)
   final inline def put(inline newState: S, inline resume: Unit => R): R = perform(StateOp.Put(newState), resume)
   final inline def update(inline upd: S => S, inline resume: Unit => R): R = get(s => put(upd(s), resume))
 }
 
 def run[K1 <: State[?, R], K2 <: State[?, R], R](
-k1: K1, k2: K2
+  k1: K1, k2: K2
 )(prog: (K1, K2) ?-> R): R = {
   prog(using k1, k2)
 }
 
-// State
-
-sealed trait StateEff[S, V]
-
+sealed trait StateOp[S, V]
 object StateOp {
-  case class Get[S]() extends StateEff[S, S]
-  case class Put[S](value: S) extends StateEff[S, Unit]
+  case class Get[S]() extends StateOp[S, S]
+  case class Put[S](value: S) extends StateOp[S, Unit]
 }
 
-trait StateCapability[S, R] extends State[S, R] with caps.SharedCapability
-
 // Prog
-
 object Sumh {
-  inline def program(using count: StateCapability[Int, Bounce[(Int, Long)]], sum: StateCapability[Long, Bounce[(Int, Long)]]): Bounce[(Int, Long)] = {
+  inline def program(using count: State[Int, Bounce[(Int, Long)]], sum: State[Long, Bounce[(Int, Long)]]): Bounce[(Int, Long)] = {
     def rec: Bounce[(Int, Long)] = {
       count.get { s =>
         count.update(_ + 1, { _ =>
           sum.update(_ + s.toLong, {_ =>
-            // if s < r then suspend[(Int, Long), {}, {}](rec) else count.get(c => result((c, s)))
             if s < 100 then suspend(rec) else count.get(c => result((c, s)))
           })
         })
@@ -51,9 +43,9 @@ object Sumh {
     rec
   }
 
-  def round1 = {
-    val state: StateCapability[Int, Bounce[(Int, Long)]] = ???
-    val sum: StateCapability[Long, Bounce[(Int, Long)]] = ???
-    val bounce = run(state, sum)(program)
+  def runProg(count: State[Int, Bounce[(Int, Long)]], sum: State[Long, Bounce[(Int, Long)]]) = {
+    val bounce = run(count, sum) { (c, s) ?=> 
+      program(using c, s)
+    }
   }
 }
