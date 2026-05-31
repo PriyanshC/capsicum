d919015745b9f45f934d2960ded0c50cb9eaeabd
Priyansh <priyanshchugh@outlook.com>
2026-05-31 16:41:23 +0100

    Better test names with classtag

--- src/test/scala/capsicum/effects/TestState.scala
+++ src/test/scala/capsicum/effects/TestState.scala
@@ -2,11 +2,11 @@ package capsicum.effects
 
 import org.scalacheck.{Arbitrary, Properties, Prop}
 import org.scalacheck.Prop.forAll
-import scala.annotation.init
+import scala.reflect.ClassTag
 
-abstract class StateLaws[S: Arbitrary](
-  newState: S => StateCapability[S, Boolean]
-) extends Properties(s"StateLaws for ${newState.getClass.getSimpleName()}") {
+abstract class StateLaws[S: Arbitrary : ClassTag, K <: StateCapability[S, Boolean]](
+  newState: S => K
+)(implicit ctag: ClassTag[K], ttag: ClassTag[S]) extends Properties(s"StateLaws for ${ctag.runtimeClass.getSimpleName()}[${ttag.runtimeClass.getSimpleName()}]") {
 
   property("Get") = forAll { (initial: S) =>
     val state = newState(initial)
@@ -48,15 +48,15 @@ abstract class StateFnLaws[S: Arbitrary](newState: =>StateCapability[S, S => S])
   }
 }
 
-def mkRWState[S: Arbitrary](s: S): StateCapability[S, Boolean] = {
-    val state = new MutableStateHandler[S, Boolean](s)
-    val r = state.asReader
-    val w = state.asWriter
-    new RWStateHandler(r, w)
+def mkRWState[S: Arbitrary](s: S): RWStateHandler[S, Boolean] = {
+  val state = new MutableStateHandler[S, Boolean](s)
+  val r = state.asReader
+  val w = state.asWriter
+  new RWStateHandler(r, w)
 }
-abstract class MutStateLaws[S: Arbitrary] extends StateLaws[S](new MutableStateHandler(_))
-abstract class PurerStateLaws[S: Arbitrary] extends StateFnLaws[S](new PurerStateCapability)
-abstract class RWStateLaws[S: Arbitrary] extends StateLaws[S](mkRWState(_))
+abstract class MutStateLaws[S: Arbitrary : ClassTag] extends StateLaws[S, MutableStateHandler[S, Boolean]](new MutableStateHandler(_))
+abstract class PurerStateLaws[S: Arbitrary : ClassTag] extends StateFnLaws[S](new PurerStateCapability)
+abstract class RWStateLaws[S: Arbitrary : ClassTag] extends StateLaws[S, RWStateHandler[S, Boolean]](mkRWState(_))
 
 object MutIntStateSpec extends MutStateLaws[Int]
 object MutStringStateSpec extends MutStateLaws[String]
