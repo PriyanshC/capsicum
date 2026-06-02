d5c3e558e4b6693b42d97e6735869612f428d703
Priyansh <priyanshchugh@outlook.com>
2026-06-02 11:37:06 +0100

    MM

---
+++ experiments/src/main/scala/motivating-examples/cc-off/Monadic.scala
@@ -0,0 +1,30 @@
+package experiments.motivation.ccoff.monadic
+
+import experiments.motivation.ccoff._
+
+case class DB[A](run: Database => A) {
+  def map[B](f: A => B): DB[B] = DB(db => f(run(db)))
+  def flatMap[B](f: A => DB[B]): DB[B] = DB(db => f(run(db)).run(db))
+}
+
+object DB {
+  def fetchNameM(id: Int): DB[String] = DB(db => db.fetchName(id))
+
+  def runTransactionM[A](action: DB[A]): A = {
+    val db = Database.openConnection()
+    val result = action.run(db)
+    db.close()
+    result
+  }
+
+  def deferFetchAllM(ids: Iterable[Int]): DB[() => Iterable[String]] = {
+    DB { db =>
+      () => ids.map(db.fetchName) 
+    }
+  }
+
+  @main def runDatabaseM() = {
+    val fetchMyNames = DB.runTransactionM(deferFetchAllM(LazyList(1, 5, 10)))
+    val names = fetchMyNames() 
+  }
+}
