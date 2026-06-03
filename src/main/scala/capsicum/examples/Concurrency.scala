08f95028284a5d1de530391ce1088b72b42e4057
Priyansh <priyanshchugh@outlook.com>
2026-06-03 10:54:35 +0100

    Curry async

--- src/main/scala/capsicum/examples/Concurrency.scala
+++ src/main/scala/capsicum/examples/Concurrency.scala
@@ -7,15 +7,11 @@ import language.experimental.captureChecking
 def basicAsyncProgram(): Unit = {
   
   def prog(using async: AsyncCapability[Unit]): Unit = {
-    async.fork(() => {
-      println("Hello from virtual thread!")
-      42
-    }, fiber => {
-      println("Forked successfully...")
-      async.join(fiber, result => {
-        println(s"Got result: $result")
-      })
-    })
+    val threadWork = () => { println("Hello from thread"); 42 }
+    async.fork(threadWork) { fiber =>
+      println("Forked")
+      async.join(fiber) { result => println(s"Got result: $result")}
+    }
   }
 
   import scala.concurrent.ExecutionContext.Implicits.global
