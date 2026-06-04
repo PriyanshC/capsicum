afccfa848c86f83c4a83cb280c6b1c3b4b84dfa1
Priyansh <priyanshchugh@outlook.com>
2026-06-04 17:19:05 +0100

    Converted std console to oneshot

--- src/main/scala/capsicum/effects/Console.scala
+++ src/main/scala/capsicum/effects/Console.scala
@@ -15,14 +15,8 @@ trait ConsoleCapability[R] extends Capability[ConsoleEff, R, R] {
   final inline def readLine(inline resume: String => R): R = perform(ConsoleOp.ReadLine(), resume)
 }
 
-class StdConsoleHandler[R] extends ConsoleCapability[R] {
-  override def perform[V](eff: ConsoleEff[V], resume: V => R): R = eff match
-  case ConsoleOp.Print(s) => {
-    scala.Console.print(s)
-    resume(())
-  }
-  case ConsoleOp.ReadLine() => {
-    val s = scala.io.StdIn.readLine()
-    resume(s)
-  }    
+class StdConsoleHandler[R] extends ConsoleCapability[R] with OneShotCapability[ConsoleEff, R, R] with KeepResult[R] {
+  override protected def handleEff[V](eff: ConsoleEff[V]): V = eff match
+  case ConsoleOp.Print(s) => scala.Console.print(s)
+  case ConsoleOp.ReadLine() => scala.io.StdIn.readLine()
 }
