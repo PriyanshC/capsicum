2cf6314d4d94df8c3b8f24724d14e97c9fc988ef
Priyansh <priyanshchugh@outlook.com>
2026-06-04 15:10:50 +0100

    Fix pacakge

--- src/main/scala/capsicum/core/Monadic.scala
+++ src/main/scala/capsicum/core/Monadic.scala
@@ -1,7 +1,5 @@
 package capsicum.core
 
-package capsicum.core
-
 extension [A, R] (f: (A => R) => R) {
   inline def flatMap[B](g: A => (B => R) => R): (B => R) => R = k => f(x => g(x)(k))
   inline def map[B](g: A => B): (B => R) => R = k => f(g andThen k)
