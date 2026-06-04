9b69cd18e050fd511181ea350b17dab04093d53e
Priyansh <priyanshchugh@outlook.com>
2026-06-04 17:06:32 +0100

    Monadic seq ext methods

---
+++ src/main/scala/capsicum/core/Monadic.scala
@@ -0,0 +1,6 @@
+package capsicum.core
+
+extension [A, R] (f: (A => R) => R) {
+  inline def flatMap[B](g: A => (B => R) => R): (B => R) => R = k => f(x => g(x)(k))
+  inline def map[B](g: A => B): (B => R) => R = k => f(g andThen k)
+}
