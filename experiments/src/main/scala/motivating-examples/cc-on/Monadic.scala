74cf80cd39a07fb6f6824b033e8ba58102667656
Priyansh <priyanshchugh@outlook.com>
2026-06-05 10:53:05 +0100

    MWithCats cc motive

--- experiments/src/main/scala/motivating-examples/cc-on/Monadic.scala
+++ experiments/src/main/scala/motivating-examples/cc-on/Monadic.scala
@@ -4,7 +4,7 @@ import experiments.motivation.ccon._
 import scala.language.experimental.captureChecking
 import scala.util.Try
 
-case class DB[A](run: Database^ => A) {
+case class DB[+A](run: Database^ => A) {
   def map[B](f: A => B): DB[B]^ = DB(db => f(run(db)))
   def flatMap[B](f: A => DB[B]): DB[B]^ = DB(db => f(run(db)).run(db))
 }
@@ -30,5 +30,33 @@ object DB {
     val fetchMyNames = DB.runTransactionM(deferFetchAllM(LazyList(1, 5, 10)))
     val names = fetchMyNames()
     println(Try(names.toList))
+  }
+}
+
+object MWithCats {
+  import cats.data.Reader
+
+  object DB {
+    type DB[A] = Reader[Database, A]
+
+    def fetchNameM(id: Int): DB[String] = 
+      Reader(db => db.fetchName(id))
+
+    def runTransactionM[A](action: DB[A]): A = {
+      val db = Database.openConnection()
+      val result = action.run(db)
+      db.close()
+      result
+    }
+
+    def deferFetchAllM(ids: Iterable[Int]): DB[() -> Iterable[String]] = {
+      Reader { db => () => ids.map(db.fetchName) }
+    }
+
+    @main def runDatabaseMCats(): Unit = {
+      val fetchMyNames = DB.runTransactionM(deferFetchAllM(LazyList(1, 5, 10)))
+      val names = fetchMyNames()
+      println(Try(names.toList))
+    }
   }
 }
