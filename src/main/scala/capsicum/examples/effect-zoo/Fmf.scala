cf6a436e89240f20244a0663eddecae356b75e38
Priyansh <priyanshchugh@outlook.com>
2026-06-07 12:11:38 +0100

    Fmf stream eff

---
+++ src/main/scala/capsicum/examples/effect-zoo/Fmf.scala
@@ -0,0 +1,20 @@
+package capsicum.examples
+
+import scala.language.experimental.captureChecking
+
+import capsicum.core._
+import capsicum.effects._
+
+object Fmf {
+  def theSeq: Seq[Int] = Seq(1, 5)
+}
+
+object Capsicum {
+  def round1 = {
+    SafeChainedStream.fromSeq(Fmf.theSeq)
+      .filter(_ % 2 == 0)
+      .map(_ + 1)
+      .fold(0)(_ + _)
+  }
+}
+
