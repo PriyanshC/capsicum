3622a5d0ac269f357297099f6c85f0c81be59f0c
Priyansh <priyanshchugh@outlook.com>
2026-06-10 14:47:47 +0100

    Pentomino impl

--- benchmarks/src/main/scala/NQueens.scala
+++ benchmarks/src/main/scala/NQueens.scala
@@ -1,47 +1,47 @@
-package capsicum.benchmarks
+// package capsicum.benchmarks
 
-import org.openjdk.jmh.annotations._
-import org.openjdk.jmh.infra.Blackhole
-import java.util.concurrent.TimeUnit
-import capsicum.examples.NQueensBacktracking
+// import org.openjdk.jmh.annotations._
+// import org.openjdk.jmh.infra.Blackhole
+// import java.util.concurrent.TimeUnit
+// import capsicum.examples.NQueensBacktracking
 
-def nQueensVanilla(n: Int): Seq[List[Int]] = {
-  def placeQueens(k: Int, queens: List[Int]): Seq[List[Int]] = {
-    if (k == n) {
-      Seq(queens.reverse)
-    } else {
-      val safeCols = (0 until n).filter(c => NQueensBacktracking.isSafe(c, queens))
-      safeCols.flatMap(col => placeQueens(k + 1, col :: queens))
-    }
-  }
+// def nQueensVanilla(n: Int): Seq[List[Int]] = {
+//   def placeQueens(k: Int, queens: List[Int]): Seq[List[Int]] = {
+//     if (k == n) {
+//       Seq(queens.reverse)
+//     } else {
+//       val safeCols = (0 until n).filter(c => NQueensBacktracking.isSafe(c, queens))
+//       safeCols.flatMap(col => placeQueens(k + 1, col :: queens))
+//     }
+//   }
 
-  placeQueens(0, Nil)
-}
+//   placeQueens(0, Nil)
+// }
 
-@State(Scope.Benchmark)
-@BenchmarkMode(Array(Mode.AverageTime))
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
-@Warmup(iterations = 10, time = 1)
-@Measurement(iterations = 10, time = 1)
-@Fork(1)
-class NQueensBenchmark {
+// @State(Scope.Benchmark)
+// @BenchmarkMode(Array(Mode.AverageTime))
+// @OutputTimeUnit(TimeUnit.MILLISECONDS)
+// @Warmup(iterations = 10, time = 1)
+// @Measurement(iterations = 10, time = 1)
+// @Fork(1)
+// class NQueensBenchmark {
 
-  @Param(Array("10", "12", "14"))
-  var n: Int = scala.compiletime.uninitialized
+//   @Param(Array("10", "12", "14"))
+//   var n: Int = scala.compiletime.uninitialized
 
-  @Setup(Level.Trial)
-  def setup(): Unit = {
-  }
+//   @Setup(Level.Trial)
+//   def setup(): Unit = {
+//   }
 
-  @Benchmark
-  def vanillaNQueens(bh: Blackhole): Unit = {
-    val result = nQueensVanilla(n)
-    bh.consume(result) 
-  }
+//   @Benchmark
+//   def vanillaNQueens(bh: Blackhole): Unit = {
+//     val result = nQueensVanilla(n)
+//     bh.consume(result) 
+//   }
 
-  @Benchmark
-  def capsicumNQueens(bh: Blackhole): Unit = {
-    val result = NQueensBacktracking.solve(n)
-    bh.consume(result)
-  }
-}
+//   @Benchmark
+//   def capsicumNQueens(bh: Blackhole): Unit = {
+//     val result = NQueensBacktracking.solve(n)
+//     bh.consume(result)
+//   }
+// }
