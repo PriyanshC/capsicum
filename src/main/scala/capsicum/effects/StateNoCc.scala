d16d8280b3d5424f91ca3e013f883ed80e08a0b1
Priyansh <priyanshchugh@outlook.com>
2026-06-11 22:36:38 +0100

    bracket

--- src/main/scala/capsicum/effects/StateNoCc.scala
+++ src/main/scala/capsicum/effects/StateNoCc.scala
@@ -6,6 +6,6 @@ import capsicum.effects._
 class SafePureStateCapability[S, T[_]] extends StateCapability[S, S => Bounce[T[S]]] {
   override def perform[V](eff: StateEff[S, V])(resume: V => (S => Bounce[T[S]])): S => Bounce[T[S]] = eff match {
       case StateOp.Get() => (currentState: S) => suspend(resume(currentState)(currentState))
-      case StateOp.Put(newState) => ((_: S) => suspend(resume(())(newState)))
+      case StateOp.Put(newState) => (_: S) => suspend(resume(())(newState))
   }
 }
