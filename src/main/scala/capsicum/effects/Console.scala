feeb454d4776c7d16d3220835e3b0db0e1e65f1a
Priyansh <priyanshchugh@outlook.com>
2026-06-12 11:05:59 +0100

    Console as enum

--- src/main/scala/capsicum/effects/Console.scala
+++ src/main/scala/capsicum/effects/Console.scala
@@ -3,20 +3,18 @@ package capsicum.effects
 import capsicum.core._
 import language.experimental.captureChecking
 
-sealed trait ConsoleEff[V] extends Effect[V]
-
-object ConsoleOp {
-  case class Print(s: String) extends ConsoleEff[Unit]
-  case class ReadLine() extends ConsoleEff[String]
+enum ConsoleEff[V] extends Effect[V] {
+  case Print(s: String) extends ConsoleEff[Unit]
+  case ReadLine() extends ConsoleEff[String]
 }
 
 trait ConsoleCapability[R] extends Capability[ConsoleEff, R, R] {
-  final inline def print(inline s: String)(inline resume: Unit => R): R = perform(ConsoleOp.Print(s))(resume)
-  final inline def readLine(inline resume: String => R): R = perform(ConsoleOp.ReadLine())(resume)
+  final inline def print(inline s: String)(inline resume: Unit => R): R = perform(ConsoleEff.Print(s))(resume)
+  final inline def readLine(inline resume: String => R): R = perform(ConsoleEff.ReadLine())(resume)
 }
 
 class StdConsoleHandler[R] extends ConsoleCapability[R] with OneShotKeepResult[ConsoleEff, R] {
   override protected def handleEff[V](eff: ConsoleEff[V]): V = eff match
-  case ConsoleOp.Print(s) => scala.Console.print(s)
-  case ConsoleOp.ReadLine() => scala.io.StdIn.readLine()
+  case ConsoleEff.Print(s) => scala.Console.print(s)
+  case ConsoleEff.ReadLine() => scala.io.StdIn.readLine()
 }
