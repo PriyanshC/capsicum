82b03ea4b6b72f99c5d804ad48ff0c3f867fbbec
Priyansh <priyanshchugh@outlook.com>
2026-06-12 11:11:08 +0100

    Async as enum

--- src/main/scala/capsicum/effects/Async.scala
+++ src/main/scala/capsicum/effects/Async.scala
@@ -6,11 +6,9 @@ import scala.concurrent.{Future, Promise, Await, ExecutionContext}
 import scala.concurrent.duration.Duration
 import scala.util.{Success, Failure}
 
-sealed trait AsyncEff[V] extends Effect[V]
-
-object AsyncOp {
-  case class Fork[T](task: () => T) extends AsyncEff[Fiber[T]]
-  case class Join[T](fiber: Fiber[T]) extends AsyncEff[T]
+enum AsyncEff[V] extends Effect[V] {
+  case Fork[T](task: () => T) extends AsyncEff[Fiber[T]]
+  case Join[T](fiber: Fiber[T]) extends AsyncEff[T]
 }
 
 opaque type Fiber[A] = Future[A]
@@ -25,12 +23,12 @@ object Fiber {
 }
 
 trait AsyncCapability[R] extends Capability[AsyncEff, R, R] {
-  final inline def fork[T](inline task: () => T)(inline resume: Fiber[T] => R): R = perform(AsyncOp.Fork(task))(resume)
-  final inline def join[T](inline fiber: Fiber[T])(inline resume: T => R): R = perform(AsyncOp.Join(fiber))(resume)
+  final inline def fork[T](inline task: () => T)(inline resume: Fiber[T] => R): R = perform(AsyncEff.Fork(task))(resume)
+  final inline def join[T](inline fiber: Fiber[T])(inline resume: T => R): R = perform(AsyncEff.Join(fiber))(resume)
 }
 
 class VirtualAsyncHandler[R](using ec: ExecutionContext) extends AsyncCapability[R] with OneShotKeepResult[AsyncEff, R] {
   override protected def handleEff[V](eff: AsyncEff[V]): V = eff match
-    case AsyncOp.Fork(task) => Fiber(Future(task()))
-    case AsyncOp.Join(fiber) => fiber.get()
+    case AsyncEff.Fork(task) => Fiber(Future(task()))
+    case AsyncEff.Join(fiber) => fiber.get()
 }
