4302c363ba954c969370260e9bab024e9f320fc1
Priyansh <84080084+PriyanshC@users.noreply.github.com>
2026-06-21 16:03:10 +0100

    Spoilers.scala

---
+++ experiments/src/main/scala/presentation/Spoilers.scala
@@ -0,0 +1,66 @@
+package capsicum.examples.presentation.spoilers
+
+trait Database {
+  def fetchName(id: Int): String // Try[String]
+  def close(): Unit
+}
+
+object SomeDatabase {
+  def openConnection(): Database = new Database {
+    private val records = Array("Jamie", "Alice", "Bob", "Charlie")
+    private var isClosed = false
+    override def fetchName(id: Int): String = {
+      if (isClosed) throw new RuntimeException("Database connection closed!")
+      records(id)
+    }
+    override def close(): Unit = isClosed = true
+  }
+
+  def withConnection[R](logic: Database => R): R = {
+    val db = openConnection()
+    val result = logic(db)
+    db.close()
+    result
+  }
+
+  def runMonad[R](logic: DB[R]): R = {
+    val db = SomeDatabase.openConnection()
+    val result = logic.run(db)
+    db.close()
+    result
+  }
+}
+
+case class DB[R](run: Database => R) // 'Reader' monad
+
+def simpleDirect(ids: List[Int]): List[String] = {
+  SomeDatabase.withConnection { db =>
+    ids.map(id => db.fetchName(id))
+  }
+}
+
+def naughtyDirect(ids: List[Int]): () => List[String] = {
+  SomeDatabase.withConnection { db =>
+    () => ids.map(id => db.fetchName(id))
+  }
+}
+
+
+def simpleMonad(ids: List[Int]): DB[List[String]] = {
+  DB { db =>
+    ids.map(id => db.fetchName(id))
+  }
+}
+
+def naughtyMonad(ids: List[Int]): DB[() => List[String]] = {
+  DB { db =>
+    () => ids.map(id => db.fetchName(id))
+  }
+}
+
+val records = List(1, 2, 3)
+
+// main
+// naughtyDirect(records)().foreach(println)
+// SomeDatabase.runMonad(naughtyMonad(records))
+
