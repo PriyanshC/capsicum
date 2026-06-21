0336dbb0a7b4fe7577efc8b07995532e99a8ade4
Priyansh <84080084+PriyanshC@users.noreply.github.com>
2026-06-21 16:09:12 +0100

    move trait

--- experiments/src/main/scala/presentation/Spoilers.scala
+++ experiments/src/main/scala/presentation/Spoilers.scala
@@ -1,9 +1,10 @@
 package capsicum.examples.presentation.spoilers
 
-trait Database {
-  def fetchName(id: Int): String // Try[String]
-  def close(): Unit
-}
+import capsicum.examples.presentation.Database
+// trait Database {
+//   def fetchName(id: Int): String // Try[String]
+//   def close(): Unit
+// }
 
 object SomeDatabase {
   def openConnection(): Database = new Database {
