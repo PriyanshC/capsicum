480490bf67f4c31144c9e98c7ad63c7b39c1af92
Priyansh <84080084+PriyanshC@users.noreply.github.com>
2026-06-23 10:03:17 +0100

    dbCap

--- experiments/src/main/scala/presentation/DatabaseCap.scala
+++ experiments/src/main/scala/presentation/DatabaseCap.scala
@@ -23,9 +23,9 @@ class DatabaseCapability[R](db: Database^) extends Capability[DatabaseEff, R, R]
 
 object DatabaseCapability {
   def withConnection[R, RR](logic: DatabaseCapability[R] ?=> RR): RR = {
-    val db = SomeDatabase.openConnection()
-    val cap = new DatabaseCapability[R](db)
-    val result = logic(using cap)
+    val db = openConnection()
+    val dbCap = new DatabaseCapability[R](db)
+    val result = logic(using dbCap)
     db.close()
     result
   }
@@ -38,11 +38,11 @@ object DatabaseCapability {
 
 
 
-// @main def main() = {
-//   val ids = List(1, 2, 3)
-//   val names = DatabaseCapability.withConnection[String, List[String]] { cap ?=>
-//     ids.map(id => cap.fetchName(id)(name => name))
-//   }
+@main def main() = {
+  val ids = List(1, 2, 3)
+  val names = DatabaseCapability.withConnection[String, List[String]] { dbCap ?=>
+    ids.map(id => dbCap.fetchName(id)(name => name))
+  }
 
-//   println(names)
-// }
+  println(names)
+}
